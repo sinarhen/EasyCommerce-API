e310a3c334726008a6ce63e7e36a5634c27c1676
sinarhen-knu <bohdan.starosivets@knu.ua>
2023-12-27 15:31:09 +0200

    ProductController skeleton

---
+++ src/Api/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using ECommerce.Models.DTOs;
+using ECommerce.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductController : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProductDto>>> GetProducts()
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetProduct(Guid id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(Product product)
+    {
+        // TODO: Implement
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(Guid id, Product product)
+    {
+        // TODO: Implement
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(Guid id)
+    {
+        // TODO: Implement
+        return Ok();
+        
+    }
+}
