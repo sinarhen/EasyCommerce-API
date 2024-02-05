6e88a65b3278a1f3dfe083a59f535db209abe44b
sinarhen <nicklchannel8@gmail.com>
2024-02-05 15:52:03 +0200

    Admin Repository skeleton

---
+++ src/Api/Data/Repositories/Admin/AdminRepository.cs
@@ -0,0 +1,35 @@
+﻿using ECommerce.Models.Entities;
+
+namespace ECommerce.Data.Repositories.Admin;
+
+public class AdminRepository: BaseRepository, IAdminRepository
+{
+    protected AdminRepository(ProductDbContext db) : base(db)
+    {
+    }
+
+    public Task<IEnumerable<User>> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUserById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task BanUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateUserRole(int id, string role)
+    {
+        throw new NotImplementedException();
+    }
+}
