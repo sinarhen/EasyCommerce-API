ad47c0b7d867b6e86281a68db75ead583272257f
sinarhen <nicklchannel8@gmail.com>
2024-02-12 21:52:50 +0200

    Added migrations for denormalization

---
+++ src/Api/Data/Migrations/20240212194422_ProductDenorm.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerce.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductDenorm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SellerId",
+                table: "Products",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SellerId",
+                table: "Products",
+                column: "SellerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_SellerId",
+                table: "Products",
+                column: "SellerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_SellerId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_SellerId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "SellerId",
+                table: "Products");
+        }
+    }
+}
