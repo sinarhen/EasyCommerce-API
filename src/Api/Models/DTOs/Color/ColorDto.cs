d1af316f0c0ae38521186ca26879f1aec2d8170a
sinarhen <nicklchannel8@gmail.com>
2024-02-15 13:28:49 +0200

    SizeDto.cs & ColorDto.cs Redundant fields removed

--- src/Api/Models/DTOs/Color/ColorDto.cs
+++ src/Api/Models/DTOs/Color/ColorDto.cs
@@ -5,8 +5,5 @@ public class ColorDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string HexCode { get; set; }
-
-    public bool? IsAvailable { get; set; }
-
-    public int? Quantity { get; set; }
+    public List<string> ImageUrls { get; set; }
 }
--- src/Api/Models/DTOs/Size/SizeDto.cs
+++ src/Api/Models/DTOs/Size/SizeDto.cs
@@ -5,8 +5,4 @@ public class SizeDto
     public Guid Id { get; set; }
     public string Name { get; set; }
     public int Value { get; set; }
-
-    public int? Quantity { get; set; }
-
-    public bool? IsAvailable { get; set; }
 }
