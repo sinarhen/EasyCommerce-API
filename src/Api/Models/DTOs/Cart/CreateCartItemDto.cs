cf2fa0101af8b196ce00ff2686abbc7570d4b5fe
sinarhen <nicklchannel8@gmail.com>
2024-02-15 11:40:43 +0200

    CreateCartItemDto.cs Added ColorId & SizeId properties

--- src/Api/Models/DTOs/Cart/CreateCartItemDto.cs
+++ src/Api/Models/DTOs/Cart/CreateCartItemDto.cs
@@ -3,6 +3,8 @@
 public class CreateCartItemDto
 {
     public Guid ProductId { get; set; }
+    public Guid ColorId { get; set; }
+    public Guid SizeId { get; set; }
     public int Quantity { get; set; }
     
 }
