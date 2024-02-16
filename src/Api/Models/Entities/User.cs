a7ac98e3ec4bad1b960a23de01636709390d58bb
sinarhen <nicklchannel8@gmail.com>
2024-02-16 10:26:31 +0200

    User.cs String length validation & removed `virtual`

--- src/Api/Models/Entities/User.cs
+++ src/Api/Models/Entities/User.cs
@@ -6,25 +6,38 @@ namespace ECommerce.Models.Entities;
 
 public class User : IdentityUser
 {
+    [StringLength(100)]
     public string FirstName { get; set; }
+    
+    [StringLength(100)]
     public string LastName { get; set; }
+    
+    [StringLength(100)]
     public string Address { get; set; }
+
+    [StringLength(100)]
     public string City { get; set; }
+    
+    [StringLength(100)]
     public string Country { get; set; }
+
+    [StringLength(10)]
     public string PostalCode { get; set; }
+    
+    [StringLength(2000)]
     public string ImageUrl { get; set; }
     public Guid? CartId { get; set; }
 
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 
-    public virtual Guid? SellerInfoId { get; set; }
+    public Guid? SellerInfoId { get; set; }
 
     // Navigation properties
     [ForeignKey("SellerInfoId")]
-    public virtual SellerInfo SellerInfo { get; set; }    
+    public SellerInfo SellerInfo { get; set; }    
     
-    public virtual BannedUser BannedUser { get; set; }
+    public BannedUser BannedUser { get; set; }
     
     public ICollection<SellerUpgradeRequest> Requests { get; set; } = new List<SellerUpgradeRequest>();
     
