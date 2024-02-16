858ff6a2628d7bb0778219e17f3f78e41e57e744
sinarhen <nicklchannel8@gmail.com>
2024-02-16 12:43:08 +0200

    WriteCategoryDto.cs Validation

--- src/Api/Models/DTOs/Category/WriteCategoryDto.cs
+++ src/Api/Models/DTOs/Category/WriteCategoryDto.cs
@@ -1,8 +1,15 @@
-﻿namespace ECommerce.Models.DTOs.Category;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.Models.DTOs.Category;
 
 public class WriteCategoryDto
 {
+    [Required]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters.")]
     public string Name { get; set; }
+    
     public Guid? ParentCategoryId { get; set; }
+    
+    [Url]
     public string ImageUrl { get; set; }
 }
