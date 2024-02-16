ad51e176170ad57bdb83f2ade5b0f353470477ff
sinarhen <nicklchannel8@gmail.com>
2024-02-16 11:21:59 +0200

    Collection.cs Validation for properties and removed `HasSale` property

--- src/Api/Models/Entities/Collection.cs
+++ src/Api/Models/Entities/Collection.cs
@@ -6,10 +6,13 @@ namespace ECommerce.Models.Entities;
 public class Collection : BaseEntity
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Name should be between 6 and 100 characters.")]
     public string Name { get; set; }
+    
+    [StringLength(3000, ErrorMessage = "Description is too long. max 3000 characters.")]
     public string Description { get; set; }
-
-    public bool HasSale { get; set; }
 
     public Guid StoreId { get; set; }
 
