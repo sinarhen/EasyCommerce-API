afc289e3e66a61a04d4746856f6d5a3cb43129cf
sinarhen <nicklchannel8@gmail.com>
2024-02-16 12:07:38 +0200

    WriteBillboardDto.cs validations

--- src/Api/Models/DTOs/Billboard/WriteBillboardDto.cs
+++ src/Api/Models/DTOs/Billboard/WriteBillboardDto.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ECommerce.Models.DTOs.Billboard;
 
 public class WriteBillboardDto
-{
+{    
+    [Required]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Title must be between 6 and 100 characters.")]
     public string Title { get; set; }
 
+    [StringLength(300, ErrorMessage = "Subtitle is too long. max 300 characters.")]
     public string Subtitle { get; set; }
 
+    [StringLength(2000, ErrorMessage = "Image URL is too long. max 2000 characters.")]
+    [Url]
     public string ImageUrl { get; set; }
 
     public Guid CollectionId { get; set; }
