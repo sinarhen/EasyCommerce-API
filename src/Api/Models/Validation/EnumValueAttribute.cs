dd2ecdb252108c1aa479d83a3cc4d03a27a13544
sinarhen <nicklchannel8@gmail.com>
2024-02-16 12:51:01 +0200

    EnumValueAttribute.cs validation for enum type

---
+++ src/Api/Models/Validation/EnumValueAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace ECommerce.Models.Validation;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+public class EnumValueAttribute : ValidationAttribute
+{
+    private readonly Type _enumType;
+
+    public EnumValueAttribute(Type enumType)
+    {
+        _enumType = enumType;
+    }
+
+    public override bool IsValid(object value)
+    {
+        if (value is not string stringValue) return false;
+
+        return Enum.TryParse(_enumType, stringValue, true, out _);
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        var validOptions = string.Join(", ", Enum.GetNames(_enumType));
+        return $"The field {name} must be one of the following: {validOptions}.";
+    }
+}
