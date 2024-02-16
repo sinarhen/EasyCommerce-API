12acd8e981f7ceaf44ac7d99140d6ac4e7b64024
sinarhen <nicklchannel8@gmail.com>
2024-02-16 12:41:27 +0200

    Validation CreateCartItemDto.cs

--- src/Api/Models/DTOs/Cart/CreateCartItemDto.cs
+++ src/Api/Models/DTOs/Cart/CreateCartItemDto.cs
@@ -1,10 +1,20 @@
-﻿namespace ECommerce.Models.DTOs.Cart;
+﻿using System.ComponentModel.DataAnnotations;
+using ECommerce.Models.Validation;
+
+namespace ECommerce.Models.DTOs.Cart;
 
 public class CreateCartItemDto
 {
+    [Required]
     public Guid ProductId { get; set; }
+    [Required]
     public Guid ColorId { get; set; }
+    
+    [Required]
     public Guid SizeId { get; set; }
+    
+    [Required]
+    [MinValue(1)]
     public int Quantity { get; set; }
     
 }
