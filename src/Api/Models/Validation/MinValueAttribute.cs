7a4fcf05e91c9dd1124997af06f23978e1e7481a
sinarhen <nicklchannel8@gmail.com>
2024-02-16 12:40:48 +0200

    Custom MinValue Attribute in Models/Validation/MinValueAttribute.cs

---
+++ src/Api/Models/Validation/MinValueAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace ECommerce.Models.Validation;
+
+using System.ComponentModel.DataAnnotations;
+
+public class MinValueAttribute : ValidationAttribute
+{
+    private readonly int _minValue;
+
+    public MinValueAttribute(int minValue)
+    {
+        _minValue = minValue;
+    }
+
+    public override bool IsValid(object value)
+    {
+        if (value is int intValue)
+        {
+            return intValue >= _minValue;
+        }
+
+        return false;
+    }
+
+    public override string FormatErrorMessage(string name)
+    {
+        return $"The field {name} must be greater than or equal to {_minValue}.";
+    }
+}
