80dc35be4cc6ba1c963373bfb1599b7687b80f4f
sinarhen <nicklchannel8@gmail.com>
2024-02-17 10:55:25 +0200

    CollectionController.cs removed exception handling

--- src/Api/Controllers/CollectionController.cs
+++ src/Api/Controllers/CollectionController.cs
@@ -22,23 +22,8 @@ public class CollectionController : GenericController
     [HttpGet]
     public async Task<ActionResult<List<CollectionDto>>> GetCollections()
     {
-        try
-        {
-            var collections = await _repository.GetRandomCollectionsAsync();
-            return Ok(_mapper.Map<List<CollectionDto>>(collections));
-        }
-        catch (ArgumentException e)
-        {
-            return BadRequest(e.Message);
-        }
-        catch (UnauthorizedAccessException e)
-        {
-            return Unauthorized(e.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, e.Message);
-        }
+        var collections = await _repository.GetRandomCollectionsAsync();
+        return Ok(_mapper.Map<List<CollectionDto>>(collections));
     }
 
     [HttpGet("{id}")]
@@ -54,33 +39,19 @@ public class CollectionController : GenericController
     [ServiceFilter(typeof(ValidationService))]
     public async Task<ActionResult> UpdateCollection(Guid id, CreateCollectionDto collectionDto)
     {
-        try
-        {
-            var ownerId = GetUserId();
-            var ownerRoles = GetUserRoles();
-            await _repository.UpdateCollectionAsync(id, collectionDto, ownerId, ownerRoles);
-            return NoContent();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var ownerId = GetUserId();
+        var ownerRoles = GetUserRoles();
+        await _repository.UpdateCollectionAsync(id, collectionDto, ownerId, ownerRoles);
+        return NoContent();
     }
 
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteCollection(Guid id)
     {
-        try
-        {
-            var ownerId = GetUserId();
-            var ownerRoles = GetUserRoles();
-            await _repository.DeleteCollectionAsync(id, ownerId, ownerRoles);
-            return NoContent();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var ownerId = GetUserId();
+        var ownerRoles = GetUserRoles();
+        await _repository.DeleteCollectionAsync(id, ownerId, ownerRoles);
+        return NoContent();
     }
 
     [HttpPost]
@@ -88,16 +59,9 @@ public class CollectionController : GenericController
     [ServiceFilter(typeof(ValidationService))]
     public async Task<ActionResult> CreateCollection(CreateCollectionDto collectionDto)
     {
-        try
-        {
-            var ownerId = GetUserId();
-            var ownerRoles = GetUserRoles();
-            var collection = await _repository.CreateCollectionAsync(collectionDto, ownerId, ownerRoles);
-            return CreatedAtAction(nameof(CreateCollection), new { id = collection.Id }, collection);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        var ownerId = GetUserId();
+        var ownerRoles = GetUserRoles();
+        var collection = await _repository.CreateCollectionAsync(collectionDto, ownerId, ownerRoles);
+        return CreatedAtAction(nameof(CreateCollection), new { id = collection.Id }, collection);
     }
 }
