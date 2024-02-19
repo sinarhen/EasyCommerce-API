c3b1d04967e1f3e6e336321cabe3b725495d69a3
sinarhen <nicklchannel8@gmail.com>
2024-02-19 17:58:36 +0200

    CreateOrder dto's

---
+++ src/Api/Models/DTOs/Order/CreateOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ECommerce.Entities.Enum;
+using ECommerce.Models.Validation;
+
+namespace ECommerce.Models.DTOs.Order;
+
+public class CreateOrderDto
+{
+    [EnumValue(typeof(OrderStatus))]
+    public string Status { get; set; }
+    
+    [Required]
+    public ICollection<CreateOrderItemDto> OrderItems { get; set; } = new List<CreateOrderItemDto>();
+}
---
+++ src/Api/Models/DTOs/Order/CreateOrderItemDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ECommerce.Models.Validation;
+
+namespace ECommerce.Models.DTOs.Order;
+
+public class CreateOrderItemDto
+{
+    [Required] public Guid ProductId { get; set; }
+
+    [Required] public Guid ColorId { get; set; }
+
+    [Required] public Guid SizeId { get; set; }
+
+    [Required] [MinValue(1)] public int Quantity { get; set; }
+   
+}
