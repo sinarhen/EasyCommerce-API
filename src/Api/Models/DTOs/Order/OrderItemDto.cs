f613e1b9d94db9eafd897598cac232b68f72cef6
sinarhen <nicklchannel8@gmail.com>
2024-02-21 19:02:35 +0200

    OrderItemDto.cs added `customer` field

--- src/Api/Models/DTOs/Order/OrderItemDto.cs
+++ src/Api/Models/DTOs/Order/OrderItemDto.cs
@@ -1,8 +1,11 @@
-﻿namespace ECommerce.Models.DTOs.Order;
+﻿using ECommerce.Models.DTOs.User;
+
+namespace ECommerce.Models.DTOs.Order;
 
 public class OrderItemDto
 {
     public Guid Id { get; set; }
     public OrderItemProductDto Product { get; set; }
     public int Quantity { get; set; }
+    public UserDto Customer { get; set; }
 }
