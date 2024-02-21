a1620c5fb68d36ad56feecb284fc8a37a0c8ce2f
sinarhen <nicklchannel8@gmail.com>
2024-02-21 19:02:51 +0200

    UserDto.cs added `PhoneNumber` property

--- src/Api/Models/DTOs/User/UserDto.cs
+++ src/Api/Models/DTOs/User/UserDto.cs
@@ -6,6 +6,7 @@ public class UserDto
     public string Username { get; set; }
     public string Email { get; set; }
     public string ImageUrl { get; set; }
+    public string PhoneNumber { get; set; }
     public string Role { get; set; }
     public List<string> Roles { get; set; }
     public bool IsBanned { get; set; }
