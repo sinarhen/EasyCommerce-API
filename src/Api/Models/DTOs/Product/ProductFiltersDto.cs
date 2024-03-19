08cd31a3a41387e87bb7f01ed4854edc740c944e
sinarhen <nicklchannel8@gmail.com>
2024-03-19 14:19:50 +0200

    ProductFiltersDto.cs for filters for all products

---
+++ src/Api/Models/DTOs/Product/ProductFiltersDto.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Models.DTOs.Category;
+using ECommerce.Models.DTOs.Color;
+using ECommerce.Models.DTOs.Material;
+using ECommerce.Models.DTOs.Size;
+
+namespace ECommerce.Models.DTOs.Product;
+
+public class ProductFiltersDto
+{
+    public List<CategoryDto> Categories;
+    public List<SizeDto> Sizes;
+    public List<ColorDto> Colors;
+    public List<IdNameDto> Occasions;
+    public List<MaterialDto> Materials;
+}
